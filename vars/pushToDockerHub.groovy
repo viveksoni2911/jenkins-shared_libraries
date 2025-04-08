8e49bffa3f0fbc819985700744159a11dc453b0a
viveksoni2911 <72186174+viveksoni2911@users.noreply.github.com>
2025-04-08 15:54:01 +0530

    Update pushToDockerHub.groovy

--- vars/pushToDockerHub.groovy
+++ vars/pushToDockerHub.groovy
@@ -1,10 +1,10 @@
 def call(){
     withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
                         sh """
-                        echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
-                        docker tag java-notes-app:latest $DOCKER_USER/java-notes-app:latest
-                        echo "docker image tag addded: $DOCKER_USER/java-notes-app:latest"
-                        docker push $DOCKER_USER/java-notes-app:latest
+                        echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin
+                        docker tag java-notes-app:latest \$DOCKER_USER/java-notes-app:latest
+                        echo "docker image tag addded: \$DOCKER_USER/java-notes-app:latest"
+                        docker push \$DOCKER_USER/java-notes-app:latest
                         """ 
     }
 }
